bce5734e499ab4446d091937b8663171c0826191
miteshmadaan <miteshmadaan1@gmail.com>
2022-02-19 18:22:34 +0530

    More DFAstate, spelling mistake in tokenTypes.txt

--- lexer.c
+++ lexer.c
@@ -72,6 +72,7 @@ void retract(int retractNum)
 TOKEN getNextTokenFromDFA(){
     char ch;
     TOKEN token;
+    int tokenReady = 0;
     while(1){
         token.lineNumber = lineNumber;
 
@@ -79,6 +80,7 @@ TOKEN getNextTokenFromDFA(){
             //cases here!
 
             case 0:
+                ch = getCharFromBuffer();
                 if((ch=='a') || ( ('e'<= ch) && (ch <= 'z')  ) ){
                     dfaState = 1;
                 }
@@ -165,19 +167,145 @@ TOKEN getNextTokenFromDFA(){
                     dfaState = 60;
                 }
                 else{
-                    dfaState = 61; //error report state new state
+                    dfaState = 61; //error report state
                 }
                 break;
                 
-                
-                
-                
+            case 24:
+                dfaState = 0;
+                token.tokenType = TK_SQL;
+                tokenReady = 1;
+                break;
 
-        }
+            case 25:
+                dfaState = 0;
+                token.tokenType = TK_SQR;
+                tokenReady = 1;
+                break;  
+
+            case 26:
+                dfaState = 0;
+                token.tokenType = TK_OP;
+                tokenReady = 1;
+                break;
+
+            case 27:
+                dfaState = 0;
+                token.tokenType = TK_CL;
+                tokenReady = 1;
+                break;  
+           
+            case 28:
+                dfaState = 0;
+                token.tokenType = TK_COMMA;
+                tokenReady = 1;
+                break;
+            
+            case 29:
+                dfaState = 0;
+                token.tokenType = TK_SEM;
+                tokenReady = 1;
+                break;
+            
+            case 30:
+                dfaState = 0;
+                token.tokenType = TK_COLON;
+                tokenReady = 1;
+                break;
+            
+            case 31:
+                dfaState = 0;
+                token.tokenType = TK_DOT;
+                tokenReady = 1;
+                break;
+
+            case 32:
+                ch = getCharFromBuffer();
+                if(ch=='\n'){
+                    lineNumber++;
+                    dfaState = 33;
+                }
+                else{
+                    dfaState = 32;
+                }
+                break;
+
+            case 33:
+                dfaState = 0;
+                break;
+
+            case 34:
+                if( (ch==' ') || (ch=='\t') || (ch=='\n') || (ch=='\r') ){
+                    dfaState = 34;
+                    if(ch=='\n'){
+                        lineNumber++;
+                    }
+                }
+                else{
+                    dfaState = 35;   
+                }
+                break;
+            
+            case 35:
+                retract(1);
+                dfaState = 0;
+                break;
+
+            case 36:
+                ch = getCharFromBuffer();
+                if(ch =='='){
+                    dfaState = 37;
+                }
+                else{
+                    retract(1);
+                    dfaState = 61; //error report state
+                }
+                break;
+            
+            case 37:
+                dfaState = 0;
+                token.tokenType = TK_NE;
+                tokenReady = 1;
+                break;
+            
+            case 38:
+                ch = getCharFromBuffer();
+                if(ch =='='){
+                    dfaState = 39;
+                }
+                else{
+                    dfaState = 40;
+                }
+                break;
+            
+            case 39:
+                dfaState = 0;
+                token.tokenType = TK_GE;
+                tokenReady = 1;
+                break;
+            
+            case 40:
+                retract(1);
+                dfaState = 0;
+                token.tokenType = TK_GT;
+                tokenReady = 1;
+                break;
 
 
 
-    }
+
+
+            case 61:
+                //report whatever from lexeme begin to just before forward pointer as an error!!
+
+            default:
+                break;   
+        }//End of switch-case
+
+        if(tokenReady ==1){}
+            return token;
+        
+    }//End of while loop
 
 
 }
