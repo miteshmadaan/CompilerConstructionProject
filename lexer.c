2e157c0ec9f1b1d666cf6c1d103120a99ea716f9
RaghavRox <raghavrox2002@gmail.com>
2022-03-03 13:46:58 +0000

    str from token type

--- lexer.c
+++ lexer.c
@@ -183,6 +183,18 @@ TOKEN makeToken(tokenType tokenTypeInput)
     
 }
 
+//get string from tokenType Enum
+char* strFromTokeType(int index)
+{
+    char* tokens[59] = {
+        "TK_ASSIGNOP","TK_COMMENT","TK_FIELDID","TK_ID","TK_NUM","TK_RNUM","TK_FUNID","TK_RUID","TK_WITH","TK_PARAMETERS","TK_END","TK_WHILE","TK_UNION","TK_ENDUNION","TK_DEFINETYPE","TK_AS","TK_TYPE","TK_MAIN","TK_GLOBAL","TK_PARAMETER","TK_LIST","TK_SQL","TK_SQR","TK_INPUT","TK_OUTPUT","TK_INT","TK_REAL","TK_COMMA","TK_SEM","TK_COLON","TK_DOT","TK_ENDWHILE","TK_OP","TK_CL","TK_IF","TK_THEN","TK_ENDIF","TK_READ","TK_WRITE","TK_RETURN","TK_PLUS","TK_MINUS","TK_MUL","TK_DIV","TK_CALL","TK_RECORD","TK_ENDRECORD","TK_ELSE","TK_AND","TK_OR","TK_NOT","TK_LT","TK_LE","TK_EQ","TK_GT","TK_GE","TK_NE","TK_ERROR","TK_EOF"
+    };
+
+    return tokens[index];
+}
+
+
+
 void tokenizeSource(){
 
     TOKEN tokenFromDfa;
@@ -218,7 +230,7 @@ void tokenizeSource(){
             {
                 printf("Lexeme %s\t" , tokenFromDfa.strLexeme);
             }
-            printf("Token %d\n" , tokenFromDfa.tokenType);
+            printf("Token %s\n" , strFromTokeType(tokenFromDfa.tokenType));
         }
     }
 
