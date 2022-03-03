529d7f6bbfb81dac234e018600d72d70b97e417b
RaghavRox <raghavrox2002@gmail.com>
2022-03-03 11:43:15 +0000

    functions

--- parser.h
+++ parser.h
@@ -4,15 +4,16 @@
 #include "defForLexer.h"
 #include "defForParser.h"
 
-int* calculateFirst(int production, Grm g, FirstSet firstSet);
+int* calculateFirst(int produc,Grm g,FirstSet firstSet);
 int* add(int* ans, int* addit);
 void buildFirstSet(Grm g, FirstSet firstSet);
 void getGram(char *fname, Grm g);
-void getFollowSets(Grm g, FollowSet fw, FirstSet fr);
-void findFollow(int NonTerminalNo, int productionNo, Grm g, FollowSet fw, FirstSet fr,int* flag);
+void getFollowSets(Grm g, FollowSet follow0, FirstSet first0);
+void findFollow(int nTerminalNum, int prodNum, Grm g, FollowSet followSet, FirstSet firstSet,int* flg2);
 void createParseTable(FirstSet firstSet,FollowSet followSet,Grm g,parseTable t);
-void add2(int* ans,int* add,int* flag);
+void add2(int* answer,int* add0,int* flg0);
 void parseInputSourceCode(FILE* sourceFile,parseTable t,Grm g,parseTree root,int* error);
 void printParseTree(parseTree root);
+void firstString(int* b,int* firstRule,int index,FirstSet firstSet);
 
 #endif
